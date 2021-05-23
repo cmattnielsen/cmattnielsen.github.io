b63fc941ac3bf3a686622a4ac18ce6c78e6aa2f7
cmattnielsen <cmattnielsen@gmail.com>
2021-05-23 00:23:17 -0600

    HeatShock

---
+++ HeatShock/Plate_Reader.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+DNA_DF <- read.csv(file = 'DNA_Conc_by_Extraction_Date.csv')
+
+
+
+
+
+
+
+
+
+colnames(airports) <- c("ID", "name", "city", "country", "IATA_FAA", "ICAO", "lat", "lon", "altitude", "timezone", "DST")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
